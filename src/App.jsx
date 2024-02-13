146307763c59dd880749c2249ad41d392038e552
RJ <jainrishi932002@gmail.com>
2024-02-13 16:16:51 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './assets/components/Experience'
+
+
+function App() {
+  
+
+  return (
+    <Canvas>
+      <Experience/>
+    </Canvas>
+  )
+}
+
+export default App
---
+++ src/assets/components/Experience.jsx
@@ -0,0 +1,15 @@
+import { OrbitControls } from "@react-three/drei"
+
+export const Experience = () => {
+    return (
+
+    <>
+        <OrbitControls />
+        <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+    </>    
+       
+    );
+}
